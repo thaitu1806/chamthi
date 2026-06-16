f81346b3316e5d620c1bf1537a62cf3b0396c894
An Tan <thainv.bp@gmail.com>
2026-06-16 22:16:39 +0700

    fix: Form Settings 540px

--- MathExamGrader/FormSettings.Designer.cs
+++ MathExamGrader/FormSettings.Designer.cs
@@ -19,7 +19,7 @@ partial class FormSettings
     {
         this.components = new System.ComponentModel.Container();
         this.Text = "⚙️ Cấu hình";
-        this.ClientSize = new Size(500, 680);
+        this.ClientSize = new Size(540, 680);
         this.StartPosition = FormStartPosition.CenterParent;
         this.FormBorderStyle = FormBorderStyle.Sizable;
         this.MaximizeBox = true;
